93765f0f673024eaa630ed8c1b43bf12c50ffa74
Aadi4321sb <85348821+Aadi4321sb@users.noreply.github.com>
2022-10-25 16:44:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+song1 = "";
+song2 = "";
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 600, 500)
+}
+function preload() {
+    song1 = loadSound("Bones.mp3");
+    song2 = loadSound("Enemy.mp3")
+}
